90d1cee4b2dd3d1322ab3454a8fac85f8b849d1e
YasinduKaveesha <yasinduksl2002@gmail.com>
2025-04-25 11:48:31 +0530

    MessageRepository.java and Message.java creation

--- backend/src/main/java/com/skillsharing/model/Message.java
+++ backend/src/main/java/com/skillsharing/model/Message.java
@@ -1,5 +1,22 @@
 package com.skillsharing.model;
 
-public class Message {
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
+}
+@Data
+@Document(collection = "messages")
+public class Message {
+    @Id
+    private String id;
+    private String senderId;
+    private String receiverId;
+    private String content;
+    private boolean read;
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
